618d720aca050021ea675bdb78e26c1796168975
fahri-r <00.fahri.r@gmail.com>
2023-09-25 08:46:15 +0700

    update homepage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,41 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary: "hsl(222.2, 47.4%, 11.2%)",
+      "primary-foreground": "hsl(210, 40%, 98%)",
+      secondary: "hsl(210, 40%, 96.1%)",
+      "secondary-foreground": "hsl(222.2, 47.4%, 11.2%)",
+      muted: "hsl(210, 40%, 96.1%)",
+      "muted-foreground": "hsl(215.4, 16.3%, 46.9%)",
+      accent: "hsl(210, 40%, 96.1%)",
+      "accent-foreground": "hsl(222.2, 47.4%, 11.2%)",
+      destructive: "hsl(0, 84.2%, 60.2%)",
+      "destructive-foreground": "hsl(210, 40%, 98%)",
+      background: "hsl(0, 0%, 100%)",
+      foreground: "hsl(222.2, 84%, 4.9%)",
+      card: "hsl(0, 0%, 100%)",
+      "card-foreground": "hsl(222.2, 84%, 4.9%)",
+      popover: "hsl(0, 0%, 100%)",
+      "popover-foreground": "hsl(222.2, 84%, 4.9%)",
+      border: "hsl(214.3, 31.8%, 91.4%)",
+      input: "hsl(214.3, 31.8%, 91.4%)",
+      ring: "hsl(222.2, 84%, 4.9%)",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
